aaa456afac922e4a34e74c4a587a7122519e05a6
Diogo Magliano <diogo.magliano@zup.com.br>
2021-11-11 22:00:57 -0300

    Adicionando validação de cartão existente e controle de paginas e numero de resultados por página (com default 10).

--- src/main/java/br/com/zupacademy/dmagliano/transacoes/transacao/TransacaoController.java
+++ src/main/java/br/com/zupacademy/dmagliano/transacoes/transacao/TransacaoController.java
@@ -2,16 +2,16 @@ package br.com.zupacademy.dmagliano.transacoes.transacao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/transacoes")
@@ -21,9 +21,19 @@ public class TransacaoController {
     TransacaoRepository transacaoRepository;
 
     @GetMapping(value = "/{idCartao}")
-    public Page<TransacaoResponseDto> listaTodasTransacoes(@PathVariable String idCartao){
+    public Page<TransacaoResponseDto> listaTodasTransacoes(@PathVariable String idCartao,
+                                                           @RequestParam(value = "page",
+                                                                   required = false,
+                                                                   defaultValue = "0") int page,
+                                                           @RequestParam(value = "size",
+                                                                   required = false,
+                                                                   defaultValue = "10") int size) {
 
-        Pageable firstPage = PageRequest.of(0, 10, Sort.by("efetivada_em").descending());
+        if (!transacaoRepository.existsByCartaoId(idCartao)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        Pageable firstPage = PageRequest.of(page, size, Sort.by("efetivada_em").descending());
         return transacaoRepository.findAllByCartaoId(idCartao, firstPage).map(TransacaoResponseDto::new);
 
     }
--- src/main/java/br/com/zupacademy/dmagliano/transacoes/transacao/TransacaoRepository.java
+++ src/main/java/br/com/zupacademy/dmagliano/transacoes/transacao/TransacaoRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransacaoRepository extends CrudRepository<Transacao, Long> {
 
     @Query(value = "SELECT * FROM transacao WHERE cartao_id = ?1",
@@ -12,4 +15,6 @@ public interface TransacaoRepository extends CrudRepository<Transacao, Long> {
             nativeQuery = true)
     Page<Transacao> findAllByCartaoId(String cartaoId, Pageable pageable);
 
+    @Query(value =  "SELECT CASE WHEN COUNT(t)> 0 THEN 'true' ELSE 'false' END FROM Transacao t WHERE t.cartao.cartaoId = :cartao_id")
+    boolean existsByCartaoId(@Param("cartao_id") String cartaoId);
 }
